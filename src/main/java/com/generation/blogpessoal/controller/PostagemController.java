bd56144d59ad613bc7a84b341b4ad020b4c17426
Isabella Rodrigues <123326628+isa01rodrigues@users.noreply.github.com>
2026-05-22 14:27:44 -0300

    Atualizações projeto

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -1,21 +1,33 @@
 package com.generation.blogpessoal.controller;
 
+
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRespository;
 
+import jakarta.validation.Valid;
+
 //Informa ao Spring que esta classe é um Controller REST. //Os métodos retornarão dados em formato JSON automaticamente.
 @RestController
 
-@RequestMapping("/postagem")  //Define a rota principal da API. //Exemplo: localhost:8080/postagem
+@RequestMapping("/postagens")  //Define a rota principal da API. //Exemplo: localhost:8080/postagem
  
 //Permite que qualquer origem acesse a API. //Isso evita problemas de CORS ao conectar front-end e back-end
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -34,9 +46,52 @@ public class PostagemController {
 	public ResponseEntity<List<Postagem>> getAll(){
 		 // Busca todas as postagens do banco usando findAll()
         // e retorna resposta HTTP 200 (OK).
-		
 		return ResponseEntity.ok(postagemRespository.findAll());
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Postagem> getById(@PathVariable Long id){
+		return postagemRespository.findById(id)
+				.map(resposta -> ResponseEntity.ok(resposta))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
 
+	@GetMapping("/titulo/{titulo}")
+	public ResponseEntity<List<Postagem>> getByTitulo(@PathVariable String titulo){
+		return ResponseEntity.ok(postagemRespository.findAllByTituloContainingIgnoreCase(titulo));
+	}
+	
+	//Representa o Insert no SQL -Inserir
+	@PostMapping
+	public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem){
+		
+		postagem.setId(null);
+		
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(postagemRespository.save(postagem));
+	}
+	
+	//Equivalente UPDATE no Sql - Atualizar
+	@PutMapping
+	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem){
+		return postagemRespository.findById(postagem.getId())
+				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
+					.body(postagemRespository.save(postagem)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		Optional<Postagem> postagem = postagemRespository.findById(id);
+		
+		if(postagem.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		postagemRespository.deleteById(id);
+		
+	}
+	
+	
 }
 
--- src/main/java/com/generation/blogpessoal/repository/PostagemRespository.java
+++ src/main/java/com/generation/blogpessoal/repository/PostagemRespository.java
@@ -2,13 +2,21 @@ package com.generation.blogpessoal.repository;
 //Define o pacote onde a interface está organizada.
 //O package ajuda a separar e estruturar os arquivos do projeto.
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 //Importa a interface JpaRepository do Spring Data JPA.
 //Ela fornece métodos prontos para manipular o banco de dados
-//como salvar, buscar, atualizar e excluir registros
+//como salvar, buscar, atualizar e excluir registro
+
+import org.springframework.data.repository.query.Param;
 
 import com.generation.blogpessoal.model.Postagem;
 //Importa a classe Postagem, que representa a entidade/tabela do banco.
+
+
+
+
 
 public interface PostagemRespository extends JpaRepository<Postagem, Long> {
 
@@ -21,4 +29,6 @@ public interface PostagemRespository extends JpaRepository<Postagem, Long> {
     // Long → tipo do atributo da chave primária (id) da entidade.
     
     // Ao herdar JpaRepository, métodos já são criados automaticamente:
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo")  String titulo);
 }
